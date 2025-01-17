03f6e0a10087edccb567918c533c8c271552b159
saikumar0605 <123saikumarreddy@gmail.com>
2025-01-17 20:59:14 +0530

    fixed issue

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name="keyword-extractor",
     version="0.0.1",
-    author="Aditya shinde",
-    author_email="shindeadi39@gmail.com",
+    author="Saikumarreddy",
+    author_email="123saikumarreddy@gmail.com",
     packages=find_packages(),
     install_requires=[],
 )
